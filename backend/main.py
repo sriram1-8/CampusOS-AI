74e4c4fed64d52e24cfc2f375dddfd39882a7a2b
sriram1-8 <sriramm.aids2025@citchennai.net>
2026-08-08 17:45:49 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "CampusOS AI is running"}
+
+@app.get("/tasks")
+def get_tasks():
+    with open("sample_data/student_data.json") as file:
+        data = json.load(file)
+
+    return data["tasks"]
